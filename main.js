d39cdd9bd01b1ae184ac5deaf64689f7eec9a720
gustavo-5G <131722213+gustavo-5G@users.noreply.github.com>
2023-12-18 17:24:18 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,43 +1,59 @@
-noseX = 0
-noseY = 0
-rightX = 0
-leftX = 0
-distancia = 0
-videoCanvas = ""
+let noseX = 0;
+let noseY = 0;
+let rightX = 0;
+let leftX = 0;
+let distancia = 0;
+let videoCanvas;
+let video;
 
-function preload(){
-videoCanvas = createVideo("./video.mp4")
-musica = loadSound("video.mp4")
+function preload() {
+  videoCanvas = createVideo("./video.mp4");
 }
 
 function setup() {
-    canvas = createCanvas(400, 400)
-    canvas.position(600, 150)
-    video = createCapture(VIDEO)
-    video.size(550, 500)
-    poseNet = ml5.poseNet(video, modelLoad)
-    poseNet.on("pose", gotPoses)
-    videoCanvas.size(400,400)
-}
+  canvas = createCanvas(400, 400);
+  canvas.position(600, 150);
 
+  // Usando createCapture para a entrada de vídeo da webcam
+  video = createCapture(VIDEO);
+  video.size(550, 500);
+
+  // Inicializando o modelo PoseNet
+  poseNet = ml5.poseNet(video, modelLoad);
+  poseNet.on("pose", gotPoses);
+
+  // Ocultando o vídeo da webcam
+  video.hide();
+
+  // Definindo o tamanho do vídeoCanvas
+  videoCanvas.size(400, 400);
+  // Inicializando o vídeo do arquivo mp4
+}
 
 function modelLoad() {
-    console.log("modelo foi iniciado")
+  console.log("Modelo foi iniciado");
 }
+
 function gotPoses(results) {
-    if (results.length > 0) {
-        console.log(results)
-        leftX = results[0].pose.leftWrist.x
-        rightX = results[0].pose.rightWrist.x
-        distancia = floor(leftX - rightX)
-        noseX = results[0].pose.nose.x
-        noseY = results[0].pose.nose.y
-    }
+  if (results.length > 0) {
+    // Obtendo as coordenadas dos pulsos e do nariz
+    leftX = results[0].pose.leftWrist.x;
+    rightX = results[0].pose.rightWrist.x;
+    distancia = floor(leftX - rightX);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+
+    // Ajustando a posição e a largura do vídeoCanvas com base nas coordenadas do nariz
+    videoCanvas.position(noseX, noseY);
+    videoCanvas.size(distancia, distancia);
+  }
 }
 
 function draw() {
-    document.getElementById("distancia").innerHTML = "Largura e Altura: "+ distancia + "px"
-    video(videoCanvas,noseX,noseY,distancia)
-    musica.play()
+  // Atualizando a largura e a posição do vídeoCanvas
+  document.getElementById("distancia").innerHTML =
+    "Largura e Altura: " + distancia + "px";
 
+  // Exibindo o vídeoCanvas
+  image(video, 0, 0, width, height);
 }
